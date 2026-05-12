f11b39b56174150d9e8b6fe9b34e4199e17490fe
TheSupereleven <mateusfernandes.ime@gmail.com>
2026-05-12 10:04:12 -0400

    Configura .gitignore e remove arquivos compilados

---
+++ src/main/model/Direcao.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Responsavel extends Pessoa {
+    private String parentesco;
+    
+    public Responsavel(String nome, String cpf, String endereco, String telefone, String email, String parentesco) {
+        super(nome, cpf, endereco, telefone, email);
+        this.parentesco = parentesco;
+    }
+    public String getParentesco() {
+        return parentesco;
+    }
+    public void setParentesco(String parentesco) {
+        this.parentesco = parentesco;
+    }
+    @Override
+    public String toString() {
+        return "Responsavel [nome=" + getNome() + ", cpf=" + getCpf() + ", endereco=" + getEndereco() + ", telefone=" + getTelefone() + ", email=" + getEmail() + ", parentesco=" + parentesco + "]";
+    }   
+}   
